57082d77a10a8402c75c9925e665132645dad071
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-10-14 13:12:25 +0530

    updated

--- YoutubeBackend/index.js
+++ YoutubeBackend/index.js
@@ -1,26 +1,30 @@
 const express = require('express');
-const app = express()
-const path = require("path")
-const fs = require('fs')
+const app = express();
+const path = require('path');
+const fs = require('fs');
 
-app.set("view engine","ejs");
-app.use(express.json())
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname,"public")));
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) =>{
-    // res.send("Welcome");
-
-    fs.readdir(`./files`, (err, files) =>{
-        // console.log(files);
-        res.render("index");
-    })
-
-
-    console.log("Done");
+app.get('/', (req, res) => {
+    const filesDirectory = path.join(__dirname, 'files');
+    
+    // Read files from the 'files' directory
+    fs.readdir(filesDirectory, (err, files) => {
+        if (err) {
+            console.error('Error reading files:', err);
+            return res.status(500).send('Internal Server Error');
+        }
+        
+        // Pass the list of files to the index.ejs view
+        res.render('index', { files });
+    });
+    
+    console.log('Request to the homepage done');
 });
 
-
-app.listen(3000, () =>{
-   console.log("Running in 3000"); 
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
 });
