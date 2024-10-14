8b016698cb5439505ce1fe0a6d86dfbf579d3427
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-10-14 16:10:34 +0530

    completed successfully

--- YoutubeBackend/index.js
+++ YoutubeBackend/index.js
@@ -8,9 +8,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const filesDirectory = path.join(__dirname, 'files');
+
+// Homepage route to render the list of files
 app.get('/', (req, res) => {
-    const filesDirectory = path.join(__dirname, 'files');
-    
     // Read files from the 'files' directory
     fs.readdir(filesDirectory, (err, files) => {
         if (err) {
@@ -21,10 +22,38 @@ app.get('/', (req, res) => {
         // Pass the list of files to the index.ejs view
         res.render('index', { files });
     });
-    
+
     console.log('Request to the homepage done');
 });
 
+// Route to create a new file
+app.post('/create', (req, res) => {
+    const { title, details } = req.body;
+
+    if (!title || !details) {
+        return res.status(400).send('Title and details are required');
+    }
+
+    // Sanitize the title for the filename (remove special characters and spaces)
+    const sanitizedTitle = title.replace(/[^a-zA-Z0-9-_ ]/g, '').trim().split(' ').join('_');
+
+    const filePath = path.join(filesDirectory, `${sanitizedTitle}.txt`);
+
+    // Write the file content
+    fs.writeFile(filePath, details, (err) => {
+        if (err) {
+            console.error('Error writing file:', err);
+            return res.status(500).send('Internal Server Error');
+        }
+
+        console.log(`File created: ${sanitizedTitle}.txt`);
+        
+        // Send success response
+        res.status(201).send('File created successfully');
+    });
+});
+
+// Start the server
 app.listen(3000, () => {
     console.log('Server running on port 3000');
 });
