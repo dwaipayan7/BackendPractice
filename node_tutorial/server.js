d7f5a11cde87aa3a672c25e2efb22f9b4e38c727
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-10-16 04:37:24 +0530

    practice

---
+++ node_tutorial/server.js
@@ -0,0 +1,62 @@
+// function add(a,b){
+//     return a+b;
+// }
+
+// var result = add(23, 55);
+// console.log(result);
+
+// (function(){
+//     console.log("Dwaipayan");
+    
+// })();
+
+//Callback Function
+// function callback(){
+//     console.log("Dwaipayan is calling");
+    
+// }
+
+
+// const add = function(a,b,callback){
+//     var result = a+b;
+//     console.log(`result: ${result}`);
+//     callback()
+// }
+
+// add(3,6, () => console.log('Dwaipayan Calling'));
+
+// var fs = require('fs')
+// var os = require('os')
+
+// var user = os.userInfo();
+// console.log(user);
+// console.log(user.username)
+
+// fs.appendFile('greeting.txt','Hi'+user.username+'!\n', ()=>{
+//     console.log('File is Created');
+// });
+
+// console.log(fs)
+
+
+const notes = require('./notes.js');
+var _ = require('lodash')
+
+
+var data = ['person', 'person', 1, 1, 2, 1, 3, 'name', 'age', '2'];
+
+var filter = _.uniq(data);
+console.log(filter)
+
+// console.log(_.isString('Dwaipayan')) -->true
+console.log(_.isString(3)) // --> false
+
+console.log("Server file is available")
+
+var age = notes.age;
+
+var result = notes.addNumber(age+14, 10);
+
+console.log(result)
+
+console.log(age+14);
