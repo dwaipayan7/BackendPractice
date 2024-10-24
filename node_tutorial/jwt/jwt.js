4b66dee8b10d21a64bb833faafe9233d9c209c3e
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-10-25 05:23:08 +0530

    jwt token created

---
+++ node_tutorial/jwt/jwt.js
@@ -0,0 +1,33 @@
+const { config } = require('dotenv');
+const jwt = require('jsonwebtoken');
+config()
+
+const jwtAuthMiddleware = (req, res, next) =>{
+
+    const token = req.headers.authorization.split(' ')[1];
+    
+    if(!token){
+        return res.status(401).json({message: 'No token provided.'});
+    }
+
+    try {
+        //payload decoded
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//Payload value
+      req.user = decoded
+      next();
+
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({message: 'Invalid token.'});
+
+    }
+}
+
+//generate token
+
+const generateToken = (userData) =>{
+    return jwt.sign({user: userData}, process.env.JWT_SECRET, {expiresIn: '1h'});
+}
+
+module.exports = {jwtAuthMiddleware, generateToken};
