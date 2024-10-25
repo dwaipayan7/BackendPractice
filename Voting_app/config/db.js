ef00640ac6a3263070d84e36ff395c5245e2080d
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-10-26 03:58:25 +0530

    project setup done

---
+++ Voting_app/config/db.js
@@ -0,0 +1,14 @@
+
+const {config} = require('dotenv');
+const mongoose = require('mongoose');
+config()
+
+const  connectDB = async () => {
+    try {
+        const mongoURL = process.env.MONGO_URL;
+        await mongoose.connect(mongoURL);
+        console.log('Connected to MongoDB server');
+    } catch (error) {
+        console.log("Error connecting to MongoDB",error);
+    }
+}
