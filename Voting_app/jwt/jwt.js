7266918922827975b953810c154fd5bd8f950b82
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-10-27 06:53:21 +0530

    successfully generated

--- Voting_app/jwt/jwt.js
+++ Voting_app/jwt/jwt.js
@@ -1,25 +1,35 @@
 const { config } = require('dotenv');
 const jwt = require('jsonwebtoken');
+config()
 
-
-// Middleware for authenticating JWT tokens
 const jwtAuthMiddleware = (req, res, next) => {
-    const token = req.headers.authorization && req.headers.authorization.split(' ')[1]; // Extract the token from the Authorization header
 
-    if (!token) {
-        return res.status(401).json({ error: 'Token not provided' });
-    }
+    // first check request headers has authorization or not
+    const authorization = req.headers.authorization
+    if(!authorization) return res.status(401).json({ error: 'Token Not Found' });
 
-    try {
-        // Verify the token and extract the payload
+    // Extract the jwt token from the request headers
+    const token = req.headers.authorization.split(' ')[1];
+    if(!token) return res.status(401).json({ error: 'Unauthorized' });
+
+    try{
+        // Verify the JWT token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.data = decoded;  // Attach the decoded payload to req.data
-        next();
-    } catch (error) {
-        return res.status(403).json({ error: 'Invalid or expired token' });
-    }
-};
 
-module.exports = {
-    jwtAuthMiddleware
-};
+        // Attach user information to the request object
+        req.user = decoded
+        next();
+    }catch(err){
+        console.error(err);
+        res.status(401).json({ error: 'Invalid token' });
+    }
+}
+
+
+// Function to generate JWT token
+const generateToken = (userData) => {
+    // Generate a new JWT token using user data
+    return jwt.sign(userData, process.env.JWT_SECRET, {expiresIn: 30000});
+}
+
+module.exports = {jwtAuthMiddleware, generateToken};
